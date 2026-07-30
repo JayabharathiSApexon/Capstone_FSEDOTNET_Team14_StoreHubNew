c18c6169cba3823bbec4dea61422ef1a10a0e662
nadiminti.ganesh <nadiminti.ganesh@ICPL15352>
2026-07-30 16:58:57 +0530

    Remove tracked bin and obj files

--- frontend/storehub-ui/src/routes/AppRoutes.tsx
+++ frontend/storehub-ui/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ProductList from "../pages/product/ProductList";
 import InventoryList from "../pages/Inventory/InventoryList";
+import OrderManagement from "../pages/order/OrderManagement";
 
 function AppRoutes() {
 
@@ -23,6 +24,11 @@ function AppRoutes() {
                 <Route
                     path="/inventory"
                     element={<InventoryList />}
+                />
+
+                <Route
+                    path="/orders"
+                    element={<OrderManagement />}
                 />
 
             </Routes>
